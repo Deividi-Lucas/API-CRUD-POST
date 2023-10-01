4fda7b3c8680a01ebd6b12e06164e1f2a250ee56
Deividi-Lucas <deividibaby6@gmail.com>
2023-09-30 20:04:16 -0400

    Chore: create validation of post

--- Program.cs
+++ Program.cs
@@ -1,18 +1,25 @@
+using System.Text.Json.Serialization;
 using dotnet_api.src.Data;
 using dotnet_api.src.Services;
 using dotnet_api.src.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    options.JsonSerializerOptions
+    .Converters.Add(new JsonStringEnumConverter()));
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<Context>(e => e.UseInMemoryDatabase("TableToDo"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IToDoServices, ToDoServices>();
+
 
 var app = builder.Build();
 
--- src/Models/ToDo.cs
+++ src/Models/ToDo.cs
@@ -6,10 +6,10 @@ public class ToDo
 {
   public int Id { get; set; }
 
-  [Required]
+  [Required(ErrorMessage = " Title is Required")]
   public string? Title { get; set; }
 
-  [Required]
+  [Required(ErrorMessage = "Description Is Required")]
   public string? Description { get; set; }
 
   [Required]
